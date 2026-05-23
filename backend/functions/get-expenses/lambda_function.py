2c7eb4f070b8915278c09f236a9e99f1c29eeb37
SamarthJayanth <Samarth10.jay@gmail.com>
2026-05-22 22:21:11 -0700

    Removed functions from get-expenses to expense-queries"

--- backend/functions/get-expenses/lambda_function.py
+++ backend/functions/get-expenses/lambda_function.py
@@ -4,33 +4,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../shared'))
 
 from input_sanitize import *
-# Need to extract expenses from specific dates
-# Extract expenses from ranges, for the month, day, above a specific amount, etc
-
-# Event will pass the type of call
-# This can be either a range of dates, after/before, above/below an amount
-def retrieve_above_amount(fields: dict):
-    amount = sanitize_amount(fields.get('amount'))
-    # Retrieve with the condition that > amount
-
-def retrieve_below_amount(fields: dict):
-    amount = sanitize_amount(fields.get('amount'))
-
-def retrieve_after_date(fields: dict):
-    start_date = sanitize_date(fields.get('date'))
-
-def retrieve_before_date(fields: dict):
-    end_date = sanitize_date(fields.get('date'))
-    # Retrieve with the condition that > date
-def retrieve_in_date_range(fields: dict):
-    start_date = sanitize_date(fields.get('start_date'))
-    end_date = sanitize_date(fields.get('end_date'))
-def retrieve_by_type(fields: dict):
-    type = sanitize_type(fields.get('type'))
-def retrieve_by_type_and_range(fields: dict):
-    type = sanitize_type(fields.get('type'))
-    start_date = sanitize_date(fields.get('start_date'))
-    end_date = sanitize_date(fields.get('end_date'))
+from expense_queries import *
 def lambda_handler(event: dict):
     fields = event.get('body')
     get_type = fields.get('type')
