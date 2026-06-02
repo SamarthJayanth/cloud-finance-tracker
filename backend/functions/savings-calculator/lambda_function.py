55e6defdbf27f52f04ed64e7e16e7c6aecc1d36d
SamarthJayanth <Samarth10.jay@gmail.com>
2026-06-02 01:27:39 -0700

    Wrote savings calculator to use the new income and expense queries functions

--- backend/functions/savings-calculator/lambda_function.py
+++ backend/functions/savings-calculator/lambda_function.py
@@ -1,30 +1,34 @@
 import json
 import uuid
-import boto3
 from datetime import date
 from decimal import Decimal # DynamoDB doesn't take float values
 from input_sanitize import * # comes from layer automatically
 from errors import * # comes from layer automatically
+from income_queries import *
+from expense_queries import *
 
-dynamodb = boto3.resource('dynamodb', region_name = 'us-east-2')
-expense_table = dynamodb.Table('expense')
-income_table = dynamodb.Table('income')
+
+
 def lambda_handler(event: dict, context):
     # Returns how much has been saved in a certain period of time
     # Date is optional
     fields = event.get('body') if event.get('body') else {}
-    start_date = fields.get('start_date')
+    start_date = fields.get('start_date') 
     end_date = fields.get('end_date')
-    if (start_date and end_date):
-        start_date = sanitize_date(start_date)
-        end_date = sanitize_date(end_date)
-        if (end_date < start_date): # We can do this because of the ordering format
-            raise ValidInputError('End date cannot be before start date')
-        income_items = get_income_in_range(start_date, end_date)
-        expense_items = get_expenses_in_range(start_date, end_date)
-    elif (start_date or end_date):
-        raise ValidInputError('Both start and end date required')
+    if(start_date and end_date):
+        total_income = get_total_income(start_date = sanitize_date(start_date), end_date = sanitize_date(end_date))
+        total_expense = get_total_expenses(start_date = sanitize_date(start_date), end_date = sanitize_date(end_date))
+    elif(start_date and not end_date):
+        total_income = get_total_income(start_date = sanitize_date(start_date))
+        total_expense = get_total_expenses(start_date = sanitize_date(start_date))
+    elif(end_date and not end_date):
+        total_income = get_total_income(end_date = sanitize_date(end_date))
+        total_expense = get_total_expenses(end_date = sanitize_date(end_date))
     else:
-        earliest_date = '2000-01-01'
-        income = get_total_income(earliest_date, str(date.today()))
-        
+        total_income = get_total_income()
+        total_expense = get_total_expenses()
+    return {
+        'body': {
+            'total_savings': total_income-total_expense
+        }
+    }
