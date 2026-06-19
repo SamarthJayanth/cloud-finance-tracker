f73a7001d2a37be73e3b314f30cd659a3e9ae5cf
SamarthJayanth <Samarth10.jay@gmail.com>
2026-06-18 19:34:05 -0700

    Added dynamodb.tf file

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,74 @@
+resource "aws_dynamodb_table" "expenses" {
+  name           = "expenses"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "user_id"
+  range_key      = "item_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "item_id"
+    type = "S"
+  }
+}
+resource "aws_dynamodb_table" "budgets" {
+  name           = "budgets"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "user_id"
+  range_key      = "item_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "item_id"
+    type = "S"
+  }
+}
+resource "aws_dynamodb_table" "incomes" {
+  name           = "incomes"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "user_id"
+  range_key      = "item_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "item_id"
+    type = "S"
+  }
+}
+resource "aws_dynamodb_table" "goals" {
+  name           = "goals"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "user_id"
+  range_key      = "item_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "item_id"
+    type = "S"
+  }
+}
+resource "aws_dynamodb_table" "plaid_items" {
+  name           = "plaid_items"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "user_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+}
