93fdcf549228f62f0daeee8205fe2c18af3d0575
SamarthJayanth <Samarth10.jay@gmail.com>
2026-06-19 23:26:06 -0700

    Created the role for add_budget, and attached necessary policies, will repeat for rest of roles next commit

--- terraform/iam_roles.tf
+++ terraform/iam_roles.tf
@@ -1,1 +1,27 @@
-#
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy" "lambda_basic_execution" {
+  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role" "add_budget" {
+  name               = "add_budget_tf"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+resource "aws_iam_role_policy_attachment" "add_budget_put_item" {
+  role       = aws_iam_role.add_budget.name
+  policy_arn = aws_iam_policy.DynamoDB_PutItem.arn
+}
+resource "aws_iam_role_policy_attachment" "add_budget_basic_execution" {
+  role       = aws_iam_role.add_budget.name
+  policy_arn = data.aws_iam_policy.lambda_basic_execution.arn
+}
